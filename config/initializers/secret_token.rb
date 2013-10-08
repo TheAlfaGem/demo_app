8ddd16afee5c5cfa4e834db7a3cc20ac6970985b
Alfa Gem <AlfaGem@techie.com>
2013-10-08 16:15:56 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '428318d06423e30404aa6dd2cb068fd0f8a9de8372bee5defecbe0c3aebcccec4f64ba4ca41e26b537e23e35ea1f52f2833ed945b4444183f47787121165d066'
